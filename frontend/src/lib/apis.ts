32fd0d9b56ef0d2d87efc8c1e172f689e36d49ae
vishalmeena2211 <meenavishal2211@gmail.com>
2024-10-08 20:59:20 +0530

    correcting the origins issue

--- frontend/src/lib/apis.ts
+++ frontend/src/lib/apis.ts
@@ -1,5 +1,5 @@
-// const baseUrl = process.env.BASE_URL ?? 'https://books-donation-assessment.onrender.com';
-const baseUrl = "http://localhost:5000";
+const baseUrl = process.env.BASE_URL ?? 'https://books-donation-assessment.onrender.com';
+// const baseUrl = "http://localhost:5000";
 
 export const authApis = {
     login: `${baseUrl}/api/v1/login`,
